3a82b98882c1798d29183c6489ef188c9ef21bd9
Lan Liang <gcslyp@gmail.com>
2022-11-24 13:16:44 +0800

    u

--- slack.sh
+++ slack.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-#$KUBECONFIG
-echo "slack.sh" $SLACK_CHANNEL
-echo "slack.sh" $SLACKT_TOKEN
-
-curl -X POST  -F file=@/tmp/admin.conf -F "init kind success!" -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload
+curl -X POST  -F file=@log -F "initial_comment=Hello, Leadville" -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload
