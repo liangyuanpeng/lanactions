6ea49cce9e8522c413579e1ac1c8f1e22929b01c
Lan Liang <gcslyp@gmail.com>
2022-12-06 11:14:45 +0800

    Add  rebuild and restore workspace

--- scripts/runopenvscode.sh
+++ scripts/runopenvscode.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.73.1/openvscode-server-v1.73.1-linux-x64.tar.gz
 tar -xf openvscode-server-v1.73.1-linux-x64.tar.gz
+rm -f openvscode-server-v1.73.1-linux-x64.tar.gz
 openvscode-server-v1.73.1-linux-x64/bin/openvscode-server --host 0.0.0.0 --without-connection-token
