186909f8c3161cb5e9a76516a3d64c9c4fb7133b
Lan Liang <gcslyp@gmail.com>
2023-11-14 15:09:25 +0800

    add sleep shell

--- actions_common_sleep.sh
+++ actions_common_sleep.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-ACTIONS_COMMON_SLEEP=${ACTIONS_COMMON_SLEEP:-"3h"}
+ACTIONS_COMMON_SLEEP=${ACTIONS_COMMON_SLEEP:-"1s"}
 echo "begin sleep "$ACTIONS_COMMON_SLEEP
 sleep $ACTIONS_COMMON_SLEEP
