8de13f7908cc65a492888090a2fff747b99880a8
Lan Liang <gcslyp@gmail.com>
2024-04-25 14:24:32 +0800

    添加kind-e2e测试

--- scripts/k8sci.sh
+++ scripts/k8sci.sh
@@ -4,7 +4,7 @@ set -o errexit;
 set -o pipefail;
 set -o nounset;
 
-
+# kind create cluster --image ghcr.io/liangyuanpeng/kindest/testnode:v0.22.0-v1.31.0-alpha.0-368-g47ad87e95fe
 function util::deployk8s(){
   STEP_WHAT=${STEP_WHAT:-"none"}
    # deployk8s, runtests
@@ -76,6 +76,14 @@ EOF
   fi
 }
 
+# Summarizing 3 Failures:
+#   [FAIL] [sig-apps] StatefulSet Deploy clustered applications [Feature:StatefulSet] [Slow] [It] should creating a working redis cluster [sig-apps, Feature:StatefulSet, Slow]
+#   k8s.io/kubernetes/test/e2e/framework/statefulset/rest.go:69
+#   [FAIL] [sig-apps] StatefulSet Deploy clustered applications [Feature:StatefulSet] [Slow] [It] should creating a working mysql cluster [sig-apps, Feature:StatefulSet, Slow]
+#   k8s.io/kubernetes/test/e2e/framework/statefulset/rest.go:69
+#   [FAIL] [sig-apps] StatefulSet Deploy clustered applications [Feature:StatefulSet] [Slow] [It] should creating a working zookeeper cluster [sig-apps, Feature:StatefulSet, Slow]
+#   k8s.io/kubernetes/test/e2e/framework/statefulset/rest.go:69
+
 function util::runtests(){
   STEP_WHAT=${STEP_WHAT:-"none"}
   TESTS_WITH=${TESTS_WITH:-"ginkgo"}
@@ -119,6 +127,12 @@ function util::runtests(){
           --dump-logs-on-failure=true                  \
           --report-dir=${PWD}/_artifacts/testreport            \
           --disable-log-dump=false
+    fi
+
+    if [ $TEST_WHAT = "kind-e2e" ];then
+      export FOCUS=.
+      
+      curl -sSL https://kind.sigs.k8s.io/dl/latest/linux-amd64.tgz | tar xvfz - -C "${PATH%%:*}/" && e2e-k8s.sh
     fi
 
   fi
