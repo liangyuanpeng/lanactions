7f41cbc09711993315b13467988f937168bfb74f
Lan <gcslyp@gmail.com>
2024-07-10 21:55:59 +0800

    Pulsar (#42)
    update, 添加lank8scn定时任务 skopeo 镜像到 liangyuanpeng

--- scripts/k8sci.sh
+++ scripts/k8sci.sh
@@ -7,14 +7,20 @@ function util::getbuild(){
   STEP_WHAT=${STEP_WHAT:-"none"}
   KIND_VERSION=${KIND_VERSION:-"v0.23.0"}
   if [ $STEP_WHAT = "getbuild" ];then 
-    wget -q https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64
+    if [ $KIND_VERSION = "latest" ];then 
+      echo "download latest version of kind."
+    else 
+      wget -q https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64
     chmod +x kind-linux-amd64 &&  mv kind-linux-amd64 /usr/local/bin/kind
+    fi
+    
   fi
 }
 
 # kind create cluster --image $KIND_IMG_REGISTRY/$KIND_IMG_USER/${KIND_IMG_REPO}:v0.22.0-v1.31.0-alpha.0-368-g47ad87e95fe
 function util::deployk8s(){
   #TODO 支持使用vagrant部署虚拟机,在虚拟机里面跑测试?
+  #TODO 部署一个staticpod, 测试过程中加入一个 etcd learner
   STEP_WHAT=${STEP_WHAT:-"none"}
 
    # deployk8s, runtests
@@ -28,7 +34,16 @@ function util::deployk8s(){
     # chmod +x /usr/local/bin/docker-compose
     # docker-compose -f docker-compose-jaeger-only.yml up -d 
 
-    export KIND_VERSION=${KIND_VERSION:-"v0.23.0"}
+    export KIND_VERSION=${KIND_VERSION:-"stable"}
+
+    if [ "$KIND_VERSION" = "stable" ];then
+      gh api repos/kubernetes-sigs/kind/tags --jq 'map(select(.name | contains("alpha") or contains("beta") | not)) | .[0].name' > kind.version
+      cat kind.version
+      # KIND_VERSION=`cat kind.version`
+      KIND_VERSION=`gh api repos/kubernetes-sigs/kind/tags --jq 'map(select(.name | contains("alpha") or contains("beta") | not)) | .[0].name'`
+      echo $KIND_VERSION
+    fi
+
     export IMGTAG=${IMGTAG:-"v1.30.0"}
     export STORAGE_MEDIA_TYPE=${STORAGE_MEDIA_TYPE:-"json"}
     export KIND_IMG_REPO=${KIND_IMG_REPO:-"kindest/testnode"}
@@ -41,6 +56,8 @@ function util::deployk8s(){
     export ENABLED_WHAT=${ENABLED_WHAT:-"default"}
     #TODO 1. 开启了apiserver-network-proxy的 k8s集群
     export ADDON_WHAT=${ADDON_WHAT:-"none"}
+
+    nohup docker pull ${KIND_IMG_REGISTRY}/${KIND_IMG_USER}/${KIND_IMG_REPO}:${KIND_VERSION}-${IMGTAG} &
 
     export ETCD_VERSION=${ETCD_VERSION:-"v3.5.14"}
     wget -q https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
@@ -294,26 +311,26 @@ function util::runtests(){
           --report-dir=${PWD}/_artifacts/testreport            \
           --disable-log-dump=true | tee ${PWD}/_artifacts/testreport/ginkgo-e2e.log
     fi
-
+    
+    #TODO test feature watchList
     
 
     
 
-    if [ $TEST_WHAT = "kind-e2e" ];then
-    #TODO 将ginkgo跑在容器里面? 或者继续研究如何才能不丢失日志 (目前在github action 会丢失ginkgo的测试日志,但是使用官方的 e2e-k8s.sh 却不会丢失,奇怪)
-    #--provider=skeleton       
-    #--prefix=e2e --network=e2e \
-      ginkgo -v --race --trace --nodes=25                \
-          --focus="."     \
-          --skip="\[Serial\]|\[sig-storage\]|\[sig-storage, Slow\]|\[sig-storage\]\[Slow\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]|PodSecurityPolicy|LoadBalancer|load.balancer|Simple.pod.should.support.exec.through.an.HTTP.proxy|subPath.should.support.existing|NFS|nfs|inline.execution.and.attach|should.be.rejected.when.no.endpoints.exist"   \
-          /usr/local/bin/e2e.test                       \
-          --                                            \
-          --kubeconfig=${PWD}/_artifacts/config     \
-          --provider=local                               \
-          --dump-logs-on-failure=true                  \
-          --report-dir=${PWD}/_artifacts/testreport            \
-          --disable-log-dump=true | tee ${PWD}/_artifacts/testreport/ginkgo-e2e.log
-    fi
+    # if [ $TEST_WHAT = "kind-e2e" ];then
+    # #--provider=skeleton       
+    # #--prefix=e2e --network=e2e \
+    #   ginkgo -v --race --trace --nodes=25                \
+    #       --focus="."     \
+    #       --skip="\[Serial\]|\[sig-storage\]|\[sig-storage, Slow\]|\[sig-storage\]\[Slow\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]|PodSecurityPolicy|LoadBalancer|load.balancer|Simple.pod.should.support.exec.through.an.HTTP.proxy|subPath.should.support.existing|NFS|nfs|inline.execution.and.attach|should.be.rejected.when.no.endpoints.exist"   \
+    #       /usr/local/bin/e2e.test                       \
+    #       --                                            \
+    #       --kubeconfig=${PWD}/_artifacts/config     \
+    #       --provider=local                               \
+    #       --dump-logs-on-failure=true                  \
+    #       --report-dir=${PWD}/_artifacts/testreport            \
+    #       --disable-log-dump=true | tee ${PWD}/_artifacts/testreport/ginkgo-e2e.log
+    # fi
 
     # [StatefulSetBasic]
     # StatefulSetStartOrdinal 
@@ -341,11 +358,11 @@ function util::runtests(){
           --disable-log-dump=true | tee ${PWD}/_artifacts/testreport/ginkgo-e2e.log
     fi
 
-    if [ $TEST_WHAT = "kind-e2e" ];then
-      export FOCUS=.
+    # if [ $TEST_WHAT = "kind-e2e" ];then
+    #   export FOCUS=.
       
-      curl -sSL https://kind.sigs.k8s.io/dl/latest/linux-amd64.tgz | tar xvfz - -C "${PATH%%:*}/" && e2e-k8s.sh
-    fi
+    #   curl -sSL https://kind.sigs.k8s.io/dl/latest/linux-amd64.tgz | tar xvfz - -C "${PATH%%:*}/" && e2e-k8s.sh
+    # fi
 
     touch ${PWD}/_artifacts/testreport/ginkgo-e2e.log
 
